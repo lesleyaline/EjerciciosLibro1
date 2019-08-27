33fc8c718e33e71126c77b2e74b9ff2ece26f3de
Lesley <lesleyperea@hotmail.com>
2019-08-27 16:55:54 -0600

    9 class segunda prte

--- class.ts
+++ class.ts
@@ -1,8 +1,8 @@
 class Shape {
     area: number;
-    color: string;
+    private color: string;
 
-    constructor (name:string, width: number, height: number){
+    constructor (public name:string, width: number, height: number){
         this.area = width * height;
         this.color = "pink";
     };
