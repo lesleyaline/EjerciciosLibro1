3627c994f0aa91ccb2f83a826ea1c59608bcce98
Lesley <lesleyperea@hotmail.com>
2019-08-27 16:53:41 -0600

    8 class

---
+++ class.ts
@@ -0,0 +1,14 @@
+class Shape {
+    area: number;
+    color: string;
+
+    constructor (name:string, width: number, height: number){
+        this.area = width * height;
+        this.color = "pink";
+    };
+  
+}
+
+
+
+
