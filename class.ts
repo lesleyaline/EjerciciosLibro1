f23c1340f02a7ecfa197bc1fde442cf1e823b6aa
Lesley <lesleyperea@hotmail.com>
2019-08-27 17:06:46 -0600

    9 class tercera parte

--- class.ts
+++ class.ts
@@ -6,8 +6,31 @@ class Shape {
         this.area = width * height;
         this.color = "pink";
     };
-  
+    shoutout(){
+        
+    }
 }
+
+class Shape3D extends Shape {
+
+    volume: number;
+    constructor (public name: string, width: number, height: number, length:number){
+        super(name,width,height);
+        this.volume =length * this.area;
+    };
+
+    shoutout(){
+        return "I'm " + this.name + " with a volume of " + this.volume + "cm cube.";
+    }
+
+    superShout(){
+        return super.shoutout();
+    }
+}
+
+var cube = new Shape3D("cube",30,30,30);
+console.log(cube.shoutout());
+console.log(cube.superShout());
 
 
 
