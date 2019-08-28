4e96f0d752fbaad9dac86e6b4703b55feb243188
Lesley <lesleyperea@hotmail.com>
2019-08-28 10:17:54 -0600

    25 tipos de datos any y never

--- datosNull.ts
+++ datosNull.ts
@@ -1,6 +1,3 @@
-12345
-enum Animals {cat = 1, lion, dog = 11, cow,
-monkey}
-let c: Animals = Animals.cat;
-console.log(Animals[3]); // undefined
-console.log(Animals.monkey); // 13
+let a: any = "apple";
+let b: any = 14;
+let c: any = false;
