b41e4ecf517823a0ff44c01df28149f608270599
Lesley <lesleyperea@hotmail.com>
2019-08-28 10:05:24 -0600

    22 tipos de datos array y tuple parte 2

--- datosNull.ts
+++ datosNull.ts
@@ -1,12 +1,7 @@
-// With strictNullChecks set to false
-let a: number[] = [1, 12, 93, 5];
-let b: string[] = ["a", "apricot", "mango"];
-let c: number[] = [1, "apple", "potato"]; // Error
-let d: Array<number> = [null, undefined, 10, 15];
-let e: Array<string> = ["pie", null, ""];
-// With strictNullChecks set to true
-let a: number[] = [1, 12, 93, 5];
-let b: string[] = ["a", "apricot", "mango"];
-let c: number[] = [1, "apple", "potato"]; // Error
-let d: Array<number> = [null, undefined, 10, 15]; // Error
-let e: Array<string> = ["pie", null, ""]; // Error
+let a: [number, string] = [11, "monday"];
+let b: [number, string] = ["monday", 11]; // Error
+let c: [number, string] = ["a", "monkey"]; // Error
+let d: [number, string] = [105, "owl", 129, 45, "cat"];
+let e: [number, string] = [13, "bat", "spiderman", 2];
+e[13] = "elephant";
+e[15] = false; // Error
