48bfef4b7c786c5c38ca3ebf06e8d4a28607d6b7
Lesley <lesleyperea@hotmail.com>
2019-08-28 10:00:58 -0600

    20 tipos de datos string parte 3

--- datosNull.ts
+++ datosNull.ts
@@ -1,4 +1,9 @@
-let e: string ="building";
-let f: number =300;
+//output in Es5
+var e= "building";
+var f=300;
+var sentence= "The " + e + "in front of my office is " + f + " feet tall.";
 
-let sentence: string =`The ${e} in front of my office is ${f} feet tall.`;
+//output in ES6
+let e ="building";
+let f =300;
+let sentence =`The ${e} in front of my office is ${f} feet tall.`;
