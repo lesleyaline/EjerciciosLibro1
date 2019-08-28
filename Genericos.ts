14a3b5a6b665369694a3c39b37d85aafc1902d21
Lesley <lesleyperea@hotmail.com>
2019-08-28 11:23:21 -0600

    41 Genericos parte 3 *

--- Genericos.ts
+++ Genericos.ts
@@ -2,5 +2,8 @@ function randomElem(theArray: any[]): any{
     let randomIndex = Math.floor(Math.random()*theArray.length);
     return theArray[randomIndex];
 }
+let positions =[103,458,472,458];
+let randomPosition = randomElem(positions);
+
 let colors:string[]= ['violet', 'indigo', 'blue','green'];
 let randomColor =randomElem(colors);
