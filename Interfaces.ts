8b8935e23bd1eaa248ab7105a7ebc55dfe367f46
Lesley <lesleyperea@hotmail.com>
2019-08-28 10:36:34 -0600

    30 interfaces parte 4

--- Interfaces.ts
+++ Interfaces.ts
@@ -4,22 +4,25 @@ interface Lakes {
     length: number,
     depth: number,
     isFreshwater: boolean,
-    countries: string[]
-}
-
-let firstLake: Lakes ={
-    name:'Caspian Sea',
-    length: 1199,
-    depth:1025,
-    area: 371000,
-    isFreshwater: false,
-    countries: ['Kazakhstan', 'Russia', 'Turkmenistan', 'Azerbaijan', 'Iran']
+    countries: string[],
+    frozen?:string[]
 }
 
 let secondLake: Lakes = {
     name: 'Superior',
+    depth: 406.3,
     length: 616,
     area: 82100,
     isFreshwater: true,
     countries: ['Canada', 'United States']
+}
+
+let thirdLake: Lakes = {
+    name: 'Baikal',
+    depth: 1637,
+    length: 636,
+    area: 31500,
+    isFreshwater: true,
+    countries: ['Russia'],
+    frozen: ['January', 'February', 'March', 'April', 'May']
 }
