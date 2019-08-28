5d7e693f5751d2ca353fc1ac6bfb6fdd25c5fae6
Lesley <lesleyperea@hotmail.com>
2019-08-28 10:07:40 -0600

    23 tipos de datos Enum

--- datosNull.ts
+++ datosNull.ts
@@ -1,7 +1,4 @@
-let a: [number, string] = [11, "monday"];
-let b: [number, string] = ["monday", 11]; // Error
-let c: [number, string] = ["a", "monkey"]; // Error
-let d: [number, string] = [105, "owl", 129, 45, "cat"];
-let e: [number, string] = [13, "bat", "spiderman", 2];
-e[13] = "elephant";
-e[15] = false; // Error
+enum Animals {cat, lion, dog, cow, monkey}
+let c: Animals = Animals.cat;
+console.log(Animals[3]); // cow
+console.log(Animals.monkey); // 4
