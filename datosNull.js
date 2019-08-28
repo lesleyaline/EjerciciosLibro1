3249f803d398497195507f3c04f13837228fc4c4
Lesley <lesleyperea@hotmail.com>
2019-08-28 09:47:48 -0600

    15 tipo de datos numericos

--- datosNull.js
+++ datosNull.js
@@ -1,4 +1,16 @@
-var a = true;
-var b = false;
-var c = 23; //error
-var d = "blue"; //error
+//with strictNullChecks set to true
+var a = undefined; //Error
+var b = null; //error
+var c = 3;
+var d = 57; //Binary
+var e = 286; //octal
+var f = 0xadf0d; //Hexadecimal
+var g = "cat"; //error
+//with structNullChecks set to false
+var a = undefined; //ok 
+var b = null; //ok
+var c = 3; //ok
+var d = 57; //Binary
+var e = 286; //octal
+var f = 0xadf0d; //hexadecimal
+var g = "cat"; //error
--- datosNull.ts
+++ datosNull.ts
@@ -1,4 +1,17 @@
-let a: boolean =true;
-let b: boolean = false;
-let c: boolean =23; //error
-let d: boolean ="blue"; //error
+//with strictNullChecks set to true
+let a: number =undefined; //Error
+let b: number =null; //error
+let c: number =3;
+let d: number = 0b111001; //Binary
+let e: number = 0o436; //octal
+let f: number = 0xadf0d; //Hexadecimal
+let g: number ="cat"; //error
+
+//with structNullChecks set to false
+let a: number =undefined; //ok 
+let b: number =null; //ok
+let c: number =3; //ok
+let d: number = 0b111001; //Binary
+let e: number = 0o436; //octal
+let f: number = 0xadf0d; //hexadecimal
+let g: number ="cat"; //error
