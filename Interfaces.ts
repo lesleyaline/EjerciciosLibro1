e9a69342c082764365cf1594e707bc7249f4954b
Lesley <lesleyperea@hotmail.com>
2019-08-28 10:41:36 -0600

    32 funciones e interfaces

--- Interfaces.ts
+++ Interfaces.ts
@@ -1,16 +1,7 @@
-interface Enemy {
-    readonly size: number,
-    health: number,
-    range: number,
-    readonly damage: number
+interface EnemyHit {
+    (name: Enemy, damageDone: number): number;
 }
-let tank: Enemy = {
-    size: 50,
-    health: 100,
-    range: 60,
-    damage: 12
+let tankHit: EnemyHit = function(tankName: Enemy, damageDone: number) {
+    tankName.health -= damageDone;
+    return tankName.health;
 }
-// This is Okay
-tank.health = 95;
-// Error because 'damage' is read-only.
-tank.damage = 10;
--- interfaces.js
+++ interfaces.js
@@ -1,10 +1,4 @@
-var tank = {
-    size: 50,
-    health: 100,
-    range: 60,
-    damage: 12
+var tankHit = function (tankName, damageDone) {
+    tankName.health -= damageDone;
+    return tankName.health;
 };
-// This is Okay
-tank.health = 95;
-// Error because 'damage' is read-only.
-tank.damage = 10;
