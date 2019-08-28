8220ab1d95fb58fa2eb2910294eb17acc9e684cb
Lesley <lesleyperea@hotmail.com>
2019-08-28 09:35:09 -0600

    12 datosNull parte 1

---
+++ datosNull.js
@@ -0,0 +1,4 @@
+var a = null; //ok
+var b = null; //error
+var c = null; //error
+var d = null; //error
---
+++ datosNull.ts
@@ -0,0 +1,6 @@
+let a: null= null; //ok
+let b: undefined = null; //error
+let c: number = null; //error
+let d: void = null; //error
+
+
