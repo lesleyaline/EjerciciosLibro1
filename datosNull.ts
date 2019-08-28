9f3b20fd4f846fe4332aa873739e58376d0133b9
Lesley <lesleyperea@hotmail.com>
2019-08-28 09:58:25 -0600

    19 tipos de datos string parte 2

--- datosNull.ts
+++ datosNull.ts
@@ -1,17 +1,4 @@
-//with strictNullChecks set to true
-let a: string =undefined; //error
-let b: string=null; //error
-let c: string= "";
-let d: string="y";
 let e: string ="building";
-let f: string =3; //error
-let g: string ="3";
+let f: number =300;
 
-//with strictNullChecks set to false
-let a: string =undefined; //ok
-let b: string=null; //ok
-let c: string= "";
-let d: string="y";
-let e: string ="building";
-let f: string =3; //error
-let g: string ="3";
+let sentence: string =`The ${e} in front of my office is ${f} feet tall.`;
