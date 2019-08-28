8edccb0fc87175ffbee032985751258c7cf4f6d7
Lesley <lesleyperea@hotmail.com>
2019-08-28 09:50:49 -0600

    16 tipo de datos numericos parte 2

--- datosNull.ts
+++ datosNull.ts
@@ -1,17 +1,7 @@
-//with strictNullChecks set to true
-let a: number =undefined; //Error
-let b: number =null; //error
-let c: number =3;
-let d: number = 0b111001; //Binary
-let e: number = 0o436; //octal
-let f: number = 0xadf0d; //Hexadecimal
-let g: number ="cat"; //error
-
-//with structNullChecks set to false
-let a: number =undefined; //ok 
-let b: number =null; //ok
-let c: number =3; //ok
-let d: number = 0b111001; //Binary
-let e: number = 0o436; //octal
-let f: number = 0xadf0d; //hexadecimal
-let g: number ="cat"; //error
+let a=undefined;
+let b=null;
+let c=3;
+let d= 0b111001;
+let e= 0o436;
+let f= 0xadf0d;  
+let g= "cat";
