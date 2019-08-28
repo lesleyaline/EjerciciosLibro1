14cada6e7b07f612bea631ba0cf79ff3287906cd
Lesley <lesleyperea@hotmail.com>
2019-08-28 10:38:33 -0600

    30 interfaces parte 4

--- Interfaces.ts
+++ Interfaces.ts
@@ -5,24 +5,18 @@ interface Lakes {
     depth: number,
     isFreshwater: boolean,
     countries: string[],
-    frozen?:string[]
+    frozen?:string[],
+    [extraProp: string]:any
 }
 
-let secondLake: Lakes = {
-    name: 'Superior',
-    depth: 406.3,
-    length: 616,
-    area: 82100,
+let fourthLake: Lakes = {
+    name: 'Tanganyika',
+    depth: 1470,
+    length: 676,
+    area: 32600,
     isFreshwater: true,
-    countries: ['Canada', 'United States']
-}
-
-let thirdLake: Lakes = {
-    name: 'Baikal',
-    depth: 1637,
-    length: 636,
-    area: 31500,
-    isFreshwater: true,
-    countries: ['Russia'],
-    frozen: ['January', 'February', 'March', 'April', 'May']
+    countries: ['Burundi', 'Tanzania', 'Zambia', 'Congo'],
+    kigoma:'Tanzania',
+    kalemie: 'Congo',
+    bujumbura: 'Burundi'
 }
---
+++ interfaces.js
@@ -0,0 +1,11 @@
+var fourthLake = {
+    name: 'Tanganyika',
+    depth: 1470,
+    length: 676,
+    area: 32600,
+    isFreshwater: true,
+    countries: ['Burundi', 'Tanzania', 'Zambia', 'Congo'],
+    kigoma: 'Tanzania',
+    kalemie: 'Congo',
+    bujumbura: 'Burundi'
+};
