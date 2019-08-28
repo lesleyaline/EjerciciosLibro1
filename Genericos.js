4ad36d3477b7d8c14a61398414103727e4d75587
Lesley <lesleyperea@hotmail.com>
2019-08-28 11:57:19 -0600

    46 Funciones genericas usando restricciones

--- Genericos.js
+++ Genericos.js
@@ -1,4 +1,8 @@
-function removeChar(theString, theChar) {
-    var theRegex = new RegExp(theChar, "gi");
-    return theString.replace(theRegex, '');
+function printName(theInput) {
+    console.log("My name is " + theInput.name);
 }
+var serena = {
+    name: 'Serena Williams',
+    profession: 'Tennis Player'
+};
+printName(serena);
--- Genericos.ts
+++ Genericos.ts
@@ -1,4 +1,20 @@
-function removeIt<T> (theInput: T, theIt: string): T{
-    let theRegex = new RegExp(theIt, "gi");
-    return theInput.replace(theRegex, '');
+interface People{
+    name: string
 }
+interface Family{
+    name:String,
+    age:number,
+    relation:string
+}
+interface Celebrity extends People{
+    profession:string
+}
+function printName<T extends People>(theInput: T): void{
+    console.log(`My name is ${theInput.name}`);
+}
+let serena: Celebrity={
+    name:'Serena Williams',
+    profession: 'Tennis Player'
+}
+
+printName(serena);
